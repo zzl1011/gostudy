de69030e458619c03d1534228b3bb199e22755b2
赵治鲁 <zhaozhilu@wepiao.com>
2017-01-17 14:20:41 +0800

    格式化

--- ex1.go
+++ ex1.go
@@ -1,23 +1,22 @@
 /**
-  *@author: zhaozhilu
-  *@source: https://leetcode.com/problems/two-sum/
-  *@return:  
-  *
-  */
+ * @author: zhaozhilu
+ * @source: https://leetcode.com/problems/two-sum/
+ *
+ */
 package main
 
 import "fmt"
 
 func main() {
-        var result [2]int
+	var result [2]int
 	target := 26
 	nums := []int{2, 7, 11, 15}
 	for i := 0; i <= len(nums)-1; i++ {
 		for j := len(nums) - 1; i < j; j-- {
 			if nums[i]+nums[j] == target {
 				fmt.Printf("The one number is %d\n, the two number is %d\n", i, j)
-				result[0]= i
-				result[1]= j
+				result[0] = i
+				result[1] = j
 			}
 		}
 	}
