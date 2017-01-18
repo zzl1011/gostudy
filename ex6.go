45bfd5ec0a291095f4a5c774aff24e44a99a2029
赵治鲁 <zhaozhilu@wepiao.com>
2017-01-18 13:53:17 +0800

    Fizz-Buzz[23]

---
+++ ex6.go
@@ -0,0 +1,27 @@
+/**
+* author: zhaozhilu
+ *
+ *
+*/
+package main
+
+import "fmt"
+
+func main() {
+	for i := 1; i <= 100; i++ {
+		if i%3 == 0 && i%5 == 0 {
+			fmt.Println("FizzBuzz")
+			continue
+		}
+		if i%3 == 0 {
+			fmt.Println("Fizz")
+			continue
+		}
+		if i%5 == 0 {
+			fmt.Println("Buzz")
+			continue
+		}
+		fmt.Println(i)
+
+	}
+}
