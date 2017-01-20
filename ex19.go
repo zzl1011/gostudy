24f92f84022db36cccf437d6163f119fb96bb224
赵治鲁 <zhaozhilu@wepiao.com>
2017-01-20 16:34:41 +0800

    传指针

---
+++ ex19.go
@@ -0,0 +1,22 @@
+/**
+ * author:zhaozhilu
+ *
+ *
+ */
+package main
+
+import "fmt"
+
+func add(a *int) int {
+	*a = *a + 1
+	return *a
+}
+
+func main() {
+	x := 3
+	fmt.Println("x = ", x)
+
+	x1 := add(&x)
+	fmt.Println("x + 1 = ", x1)
+	fmt.Println("x = ", x)
+}
