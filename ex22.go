0e4476200bc419372793d13bb81fedceb1699aab
赵治鲁 <zhaozhilu@wepiao.com>
2017-01-23 15:25:13 +0800

    面向对象-方法继承

---
+++ ex22.go
@@ -0,0 +1,38 @@
+/**
+ * author:zhaozhilu
+ * 面向对象-继承
+ *
+ */
+package main
+
+import "fmt"
+
+type Human struct {
+	name  string
+	age   int
+	phone string
+}
+
+type Student struct {
+	Human
+	school string
+}
+
+type Employee struct {
+	Human
+	company string
+}
+
+//工作类型上的函数
+//(h *Human) SayHi()类似： var h *Human  h.SayHi()
+func (h *Human) SayHi() {
+	fmt.Printf("Hi, I am %s you can call me on %s\n", h.name, h.phone)
+}
+
+func main() {
+	mark := Student{Human{"Mark", 25, "222-222-YYYY"}, "MIT"}
+	sam := Employee{Human{"Sam", 45, "111-888-XXXX"}, "Golang Inc"}
+
+	mark.SayHi()
+	sam.SayHi()
+}
