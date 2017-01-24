3a59134e6b04c5b18f4c6b3348ac242af20ee2b1
赵治鲁 <zhaozhilu@wepiao.com>
2017-01-24 14:43:21 +0800

    Errorf

---
+++ 25.go
@@ -0,0 +1,18 @@
+/**
+ * author ：zhaozhilu
+ * package: errors - Errorf
+ *
+ */
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	const name, id = "zhaozhilu", 17
+	err := fmt.Errorf("user %q(id %d) not found", name, id)
+	if err != nil {
+		fmt.Println(err) //这里不能使用printf
+	}
+}
