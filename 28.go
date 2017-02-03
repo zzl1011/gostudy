19f9a6ff4ab62de23ee4d8885a8ea483f817aa85
赵治鲁 <zhaozhilu@wepiao.com>
2017-02-03 17:38:56 +0800

    闭包实现斐波纳契数列

---
+++ 28.go
@@ -0,0 +1,25 @@
+/**
+ * author: zhaozhilu
+ * 闭包实现斐波那契
+ *
+ */
+package main
+
+import (
+	"fmt"
+)
+
+func fib() func() int {
+	a, b := 0, 1
+	return func() int {
+		a, b = b, a+b
+		return a
+	}
+
+}
+
+func main() {
+	f := fib()
+
+	fmt.Println(f(), f(), f(), f(), f(), f(), f())
+}
