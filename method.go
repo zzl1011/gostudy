ef673c223ac55fb8a48ab946f24213858b60f910
赵治鲁 <zhaozhilu@wepiao.com>
2017-02-21 10:53:49 +0800

    method

---
+++ method.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type myTime struct {
+	time.Time
+}
+
+func (t myTime) first3Chars() string {
+	return t.Time.String()[0:3]
+}
+
+func main() {
+	m := myTime{time.Now()}
+	fmt.Println("Full time now:", m.String(), m.Year(), m.Month(), m.Day())
+	fmt.Println("First 3 chars:", m.first3Chars())
+}
